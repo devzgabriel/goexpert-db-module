7a9dd759b1f4a1ebce01e33d0bf3fda201d6c8fa
Gabriel Silva <devzgabriel@gmail.com>
2025-02-20 18:38:07 -0300

    build: start project

---
+++ client/go.mod
@@ -0,0 +1,3 @@
+module github.com/devzgabriel/goexpert-db-module/client
+
+go 1.23.0
---
+++ server/server.go
@@ -0,0 +1,7 @@
+package main
+
+var sourceUrl = "https://economia.awesomeapi.com.br/json/last/USD-BRL"
+
+func main() {
+	println(sourceUrl)
+}
